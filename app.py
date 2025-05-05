8446e04b481c2ab176c062aa9c743f1f1954884f
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-05-05 12:52:38 +1200

    tested datebase

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print("hello world")
+import sqlite3
+
+db = sqlite3.connect("mlbb.db")
+cursor = db.cursor()
+sql = "SELECT * from mlbb;"
+cursor.execute(sql)
+results = cursor.fetchall()
+print(results)
+db.close()
